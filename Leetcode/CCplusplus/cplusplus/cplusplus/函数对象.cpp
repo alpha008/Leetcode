c1af53786e8696cc27f5b2eaf82cc2454b96a5f5
alpha <zjxlove2017@163.com>
2020-10-03 22:22:25 +0800

    c语言100个小例子

---
+++ Leetcode/CCplusplus/cplusplus/cplusplus/函数对象.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<deque>
+#include<list>
+#include<set>
+#include<map>
+#include<unordered_set>
+#include<unordered_map>
+#include<algorithm>
+using namespace std;
+
+
+void main()
+{
+
+	system("pause");
+}
